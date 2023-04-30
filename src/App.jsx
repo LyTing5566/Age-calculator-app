14fa66135ca6afd0e5e22f6191520f8ccc61dba9
lyting5566 <dcr957351@gmail.com>
2023-05-01 01:47:57 +0800

    feat:adjust html

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,6 @@ function App() {
   }
 
   return (
-    //
     <div
       className=" bg-white w-[860px] h-[651px] mx-auto mt-10 p-14"
       style={{
@@ -52,10 +51,10 @@ function App() {
       }}
     >
       <div className="flex gap-8">
-        <div className="flex flex-col ">
+        <div>
           <label
             htmlFor=""
-            className="text-[#808080] border-[#716F6F] text-[14px] mb-2"
+            className="text-[#808080] border-[#716F6F] text-[14px] mb-2 block"
           >
             DAY
           </label>
@@ -67,10 +66,10 @@ function App() {
           />
         </div>
 
-        <div className="flex flex-col ">
+        <div>
           <label
             htmlFor=""
-            className="text-[#808080] border-[#716F6F] text-[14px] mb-2"
+            className="text-[#808080] border-[#716F6F] text-[14px] mb-2 block"
           >
             MONTH
           </label>
@@ -82,10 +81,10 @@ function App() {
           />
         </div>
 
-        <div className="flex flex-col ">
+        <div>
           <label
             htmlFor=""
-            className="text-[#808080] border-[#716F6F] text-[14px] mb-2"
+            className="text-[#808080] border-[#716F6F] text-[14px] mb-2 block"
           >
             YEAR
           </label>
@@ -99,36 +98,33 @@ function App() {
       </div>
 
       <div className="flex items-center">
-        <div>
-          <hr className="bg-[#50d71e] border w-[632px]" />
-        </div>
-        <div className="">
-          <button
-            onClick={() => calculateAge(year, month, day)}
-            className=" self-center"
+        <hr className="bg-[#50d71e] border w-[632px]" />
+
+        <button
+          onClick={() => calculateAge(year, month, day)}
+          className=" self-center"
+        >
+          <svg
+            width="96"
+            height="96"
+            viewBox="0 0 96 96"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            <svg
-              width="96"
-              height="96"
-              viewBox="0 0 96 96"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <rect width="96" height="96" rx="48" fill="#854DFF" />
-              <path
-                d="M26 48.0189C33.3333 47.6859 48 51.6158 48 70"
-                stroke="white"
-                strokeWidth="2"
-              />
-              <path d="M48 70V26" stroke="white" strokeWidth="2" />
-              <path
-                d="M70 48.0189C62.6667 47.6859 48 51.6158 48 70"
-                stroke="white"
-                strokeWidth="2"
-              />
-            </svg>
-          </button>
-        </div>
+            <rect width="96" height="96" rx="48" fill="#854DFF" />
+            <path
+              d="M26 48.0189C33.3333 47.6859 48 51.6158 48 70"
+              stroke="white"
+              strokeWidth="2"
+            />
+            <path d="M48 70V26" stroke="white" strokeWidth="2" />
+            <path
+              d="M70 48.0189C62.6667 47.6859 48 51.6158 48 70"
+              stroke="white"
+              strokeWidth="2"
+            />
+          </svg>
+        </button>
       </div>
 
       <div className="font-extrabold" style={textShadow}>
