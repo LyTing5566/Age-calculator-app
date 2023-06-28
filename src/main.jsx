8050f163789363e719c90c8ddbc734fcf1baf494
lyting5566 <dcr957351@gmail.com>
2023-06-29 00:06:04 +0800

    add global css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '../dist/output.css';
+import '../src/output.css';
 import App from './App.jsx';
 import './index.css';
 
